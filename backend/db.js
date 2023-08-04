f5ba6abe8d088f4178bfee57c5ef3378d209cf55
manasagar <manasagarwal12921@gmail.com>
2023-08-04 18:05:08 +0530

    2

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,10 @@
 const mongoose=require('mongoose')
 const mongourl='mongodb+srv://manas:manas@cluster0.osifiku.mongodb.net/project'
 const connectToMongo=()=>{
-    mongoose.connect(mongourl,()=>{
-        console.log("connected to database")
-    })
+    mongoose.connect(mongourl,{useNewUrlParser:true,useUnifiedTopology:true})
+   let db= mongoose.connection
+   db.once('open',function(){
+   console.log('connected to database')
+})
 }
+module.exports=connectToMongo;
