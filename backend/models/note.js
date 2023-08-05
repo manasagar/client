8bcea6fafafda2b51edd5f439e7e6ea6c59c6e0a
manasagar <manasagarwal12921@gmail.com>
2023-08-05 15:43:21 +0530

    4

--- backend/models/note.js
+++ backend/models/note.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+
 const noteSchema = new mongoose.Schema({
+    user:{type:mongoose.Schema.Types.ObjectId,
+    ref:'user'},
     title: String,
     description: String,
    
--- backend/routes/note.js
+++ backend/routes/note.js
@@ -1,6 +1,45 @@
 const express = require('express')
 const router = express()
-router.get('/',(req,res)=>{
-
+const { validationResult, body } = require('express-validator');
+const Note=require('../models/note')
+const fetchuser=require('../middleware/fetch')
+//fetch notes route 1
+router.get('/',async (req,res)=>{
+res.send('manas')
 })
+router.get('/fetchallnotes',fetchuser,async (req,res)=>{
+    try{
+const notes=await Note.find({user:req.user.id});
+res.json(notes)
+    }
+    catch(error){
+        console.error(error.message)
+        res.status(500).send('internal server error');
+    }
+})
+//fetch notes route 2
+router.post('/createnote',fetchuser,[
+    body('title','no title found').exists(),
+  body('description','too short').isLength({ min: 6 })
+],async (req,res)=>{
+    const errors = validationResult(req);
+    try{
+
+    
+if(!errors.isEmpty()){
+    return res.status(400).json({errors:error.array()});
+    
+}const note=new Note({
+    'user':req.user.id,
+    'title':req.body.title,
+    'description':req.body.title,
+})
+const savednote=await note.save();
+res.json(savednote);
+    }catch(error){
+        console.error(error.message)
+        res.status(500).send('internal server error');
+    }
+    })
+
 module.exports=router
